c7a5655f93db2647e211ed00f9e5b3fc895ab9ad
Bruno Mogetta <brunomogetta@gmail.com>
2022-12-08 13:09:19 -0300

    feat: badges.sh

--- .github/scripts/change-package.sh
+++ .github/scripts/change-package.sh
@@ -22,6 +22,3 @@ JQ_OUTPUT=`jq \
 
 # Overwrite package.json
 echo "$JQ_OUTPUT" > package.json
-
-# Rename instances of "paulrberg/hardhat-template" to the new repo name in README.md for badges only
-#sed -i -e "/Use this template/! s|paulrberg/hardhat-template|"${GITHUB_REPOSITORY}"|;" "README.md"
